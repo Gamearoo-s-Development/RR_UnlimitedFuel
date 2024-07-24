7dbc8e842bd388bc452576f73eb288ef40e862a1
Gamearoo <gamearoo@gamearoo.dev>
2024-07-24 15:02:06 -0400

    initial

---
+++ FuelPatchDiesel.cs
@@ -0,0 +1,79 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Model;
+using Game.State;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Linq;
+
+namespace UnlimitedFuel
+{
+    [HarmonyPatch(typeof(DieselLocomotive))]
+    [HarmonyPatch("PeriodicUpdate")]
+    class FuelPatchDiesel
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            var methodToCall = typeof(DieselLocomotive).BaseType.GetMethod("PeriodicUpdate", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            if (methodToCall == null)
+            {
+                Debug.LogError("Base PeriodicUpdate method not found.");
+                return instructions;
+            }
+
+            var newInstructions = new List<CodeInstruction>();
+            var labelOriginalCode = il.DefineLabel();
+            var labelEndOfMethod = il.DefineLabel();
+
+            // Add instructions to call CheckCondition at the start of the method
+            newInstructions.Add(new CodeInstruction(OpCodes.Ldarg_0)); // Load "this"
+            newInstructions.Add(new CodeInstruction(OpCodes.Call, typeof(FuelPatchSteam).GetMethod(nameof(CheckCondition), BindingFlags.Static | BindingFlags.NonPublic))); // Call CheckCondition
+            newInstructions.Add(new CodeInstruction(OpCodes.Brfalse, labelOriginalCode)); // If false, jump to original code
+
+            // Insert additional behavior if CheckCondition is true
+            newInstructions.Add(new CodeInstruction(OpCodes.Ldarg_0)); // Load "this"
+            newInstructions.Add(new CodeInstruction(OpCodes.Ldarg_1)); // Load "dt"
+            newInstructions.Add(new CodeInstruction(OpCodes.Call, methodToCall)); // Call base method
+            newInstructions.Add(new CodeInstruction(OpCodes.Ldarg_0)); // Load "this"
+            newInstructions.Add(new CodeInstruction(OpCodes.Call, typeof(FuelPatchSteam).GetMethod(nameof(AdditionalBehavior), BindingFlags.Static | BindingFlags.NonPublic))); // Call AdditionalBehavior
+            newInstructions.Add(new CodeInstruction(OpCodes.Br, labelEndOfMethod)); // Jump to the end of the method
+
+            // Mark the label for original code
+            newInstructions.Add(new CodeInstruction(OpCodes.Nop) { labels = new List<Label> { labelOriginalCode } });
+
+            // Add the rest of the original instructions
+            newInstructions.AddRange(codes);
+
+            // Mark the end of the method
+            newInstructions.Add(new CodeInstruction(OpCodes.Nop) { labels = new List<Label> { labelEndOfMethod } });
+
+            return newInstructions.AsEnumerable();
+        }
+
+        static bool CheckCondition(DieselLocomotive __instance)
+        {
+            PlayersManager playersManager = GameObject.FindObjectOfType<PlayersManager>();
+            if (playersManager == null)
+            {
+                Debug.LogError("PlayersManager instance not found.");
+                return false;
+            }
+
+            string crewName = playersManager.NameForTrainCrewId(__instance.trainCrewId);
+            Debug.Log($"Crew Name: {crewName}");
+
+            return !crewName.StartsWith("FUEL");
+        }
+
+        static void AdditionalBehavior(DieselLocomotive __instance)
+        {
+            Debug.Log("Executing additional behavior for DieselLocomotive.");
+            // Add your custom behavior here
+        }
+    }
+
+}
---
+++ Main.cs
@@ -0,0 +1,65 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using UnityModManagerNet;
+
+namespace UnlimitedFuel
+{
+    static class Main
+    {
+        private static bool enabled;
+        private static UnityModManager.ModEntry myModEntry;
+        private static Harmony myHarmony;
+
+        private static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                myModEntry = modEntry;
+                modEntry.OnToggle = OnToggle;
+                modEntry.OnUnload = OnUnload;
+
+                myHarmony = new Harmony(myModEntry.Info.Id);
+                myHarmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                myModEntry.Logger.LogException($"Failed to load {myModEntry.Info.DisplayName}:", ex);
+                myHarmony?.UnpatchAll(myModEntry.Info.Id);
+                return false;
+            }
+
+            modEntry.Logger.Log("Loaded");
+
+            return true;
+        }
+
+        private static bool OnUnload(UnityModManager.ModEntry modEntry)
+        {
+            myHarmony?.UnpatchAll(myModEntry.Info.Id);
+            return true;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            if (value == enabled) return true; // No change in state
+
+            enabled = value;
+
+            if (enabled)
+            {
+                // Reapply patches when enabled
+                myHarmony.PatchAll(Assembly.GetExecutingAssembly());
+                modEntry.Logger.Log("Hello!");
+            }
+            else
+            {
+                // Remove patches when disabled
+                myHarmony.UnpatchAll(myModEntry.Info.Id);
+                modEntry.Logger.Log("Goodbye!");
+            }
+
+            return true;
+        }
+    }
+}
